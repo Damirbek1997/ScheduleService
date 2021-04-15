4ce2f1b772a40315b5244372e82a01b749a40d89
damirbek1997 <damir.1997@mail.ru>
2021-04-15 19:49:46 +0600

    little changes

---
+++ src/main/java/com/example/scheduleservice/configs/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.scheduleservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("http://localhost");
+    }
+}
