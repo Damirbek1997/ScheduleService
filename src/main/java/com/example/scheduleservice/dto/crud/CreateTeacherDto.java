c83f3a08e3f64f77168edbf01094b02aa88f17eb
damirbek1997 <damir.1997@mail.ru>
2021-04-30 19:08:38 +0600

    asdas

--- src/main/java/com/example/scheduleservice/dto/crud/CreateTeacherDto.java
+++ src/main/java/com/example/scheduleservice/dto/crud/CreateTeacherDto.java
@@ -1,17 +1,9 @@
 package com.example.scheduleservice.dto.crud;
 
-import com.example.scheduleservice.dto.SubjectDto;
-import com.example.scheduleservice.dto.UserDto;
-import com.sun.istack.NotNull;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class CreateTeacherDto {
     private String firstname;
     private String lastname;
-
-    @NotNull
-    private List<SubjectDto> subjectDtos;
 }
--- src/main/java/com/example/scheduleservice/dto/crud/UpdateTeacherDto.java
+++ src/main/java/com/example/scheduleservice/dto/crud/UpdateTeacherDto.java
@@ -1,15 +1,10 @@
 package com.example.scheduleservice.dto.crud;
 
-import com.example.scheduleservice.dto.SubjectDto;
-import com.example.scheduleservice.dto.UserDto;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class UpdateTeacherDto {
     private Long id;
     private String firstname;
     private String lastname;
-    private List<SubjectDto> subjectDtos;
 }
