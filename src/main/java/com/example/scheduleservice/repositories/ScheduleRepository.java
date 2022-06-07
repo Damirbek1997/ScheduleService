4dae1dcf7cd706a4b0cd667b2ee1596f4e40938b
Damirbek <49182450+Damirbek1997@users.noreply.github.com>
2022-06-07 11:59:40 +0600

    Update ScheduleRepository.java
    asd

--- src/main/java/com/example/scheduleservice/repositories/ScheduleRepository.java
+++ src/main/java/com/example/scheduleservice/repositories/ScheduleRepository.java
@@ -3,6 +3,7 @@ package com.example.scheduleservice.repositories;
 import com.example.scheduleservice.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
